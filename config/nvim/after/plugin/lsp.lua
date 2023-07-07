39a3d943ce520e79d80c69c60a00773b5914aef4
Tim Gesekus <tim.gesekus@gmail.com>
2023-07-08 00:08:10 +0200

    Lsp 2.0

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,12 +1,10 @@
--- Learn the keybindings, see :help lsp-zero-keybindings
--- Learn to configure LSP servers, see :help lsp-zero-api-showcase
--- local lsp = require('lsp-zero')
--- lsp.preset('recommended')
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
 
 -- (Optional) Configure lua language server for neovim
--- lsp.nvim_workspace()
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
--- require'lspconfig'.unison.setup{}
--- lsp.setup()
-
-
+lsp.setup()
